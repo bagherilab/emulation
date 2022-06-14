35a01b9004a3890fc9d156607f15a9d1af0b0ffe
Jason Cain <jcain@uw.edu>
2022-06-14 11:39:19 -0500

    added anticipated entrypoint

---
+++ main.py
@@ -0,0 +1,29 @@
+# Log configuration
+LOG_PATH = "./runs"
+
+# Data configuration
+DATA_DIR = "./data/raw"
+
+# Case Study 1
+def finley_case_study():
+    raise NotImplementedError()
+
+
+# Case Study 2
+def tumorcode_case_study():
+    raise NotImplementedError()
+
+
+# Case Study 3
+def arcade_case_study():
+    raise NotImplementedError()
+
+
+def main():
+    finley_case_study()
+    tumorcode_case_study()
+    arcade_case_study()
+
+
+if __name__ == "__main__":
+    main()
