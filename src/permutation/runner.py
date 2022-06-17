5c762625e52f9cc377bb0db5fe3fb454f5f09dea
Jason Cain <jcain@uw.edu>
2022-06-16 22:28:10 -0500

    simplified runner

--- src/permutation/runner.py
+++ src/permutation/runner.py
@@ -1,6 +1,7 @@
 from typing import Any, Optional, Tuple
 
 import sklearn as sk
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 from permutation.metrics import BatchMetric, SequentialMetric
@@ -16,92 +17,40 @@ class Runner:
     Runner contains the data from multiple runs of the same model with the same hyperparameters.
     """
 
-    def __init__(
-        self,
-        model: Model,
-        loader: Loader,
-        hparams: Optional[Hyperparams] = None,
-        subsample_n: Optional[int] = None,
-    ) -> None:
+    def __init__(self, model: Model, loader: Loader) -> None:
         self.model = model
         self.loader = loader
 
+        self.cv_metrics: BatchMetric = None
         self.training_metrics: SequentialMetric = None
-        self.test_metrics: BatchMetric = None
+        self.test_metrics: SequentialMetric = None
         self.permutation_metrics: BatchMetric = None
 
-        self.X: pd.DataFrame = None
-        self.y: pd.Series = None
+        self.stage = Stage.TRAIN if self.model.hparams is None else Stage.VAL
 
-        self.X_train: pd.DataFrame = None
-        self.X_test: pd.DataFrame = None
-        self.y_train: pd.Series = None
-        self.y_test: pd.Series = None
-
-        self.stage = Stage.TRAIN if hparams is None else Stage.VAL
-        self.hparams = hparams
-
-    def load_data(self, features: list[str], response: str, test_size=0.3, seed=100):
-        temp_df = self.loader.load()
-
-        if subsample_n:
-            temp_df = self.subsample_data(temp_df, self.subsample_n)
-
-        self.X, self.y = features_response_split(temp_df, features, response)
-
-        self.X_train, self.X_test, self.y_train, self.y_test = split(
-            self.X, self.y, test_size, seed
-        )
-
-    def unload_data(self) -> None:
-        self.X = None
-        self.y = None
-        self.X_train = None
-        self.X_test = None
-        self.y_train = None
-        self.y_test = None
-
-    def cross_validation(self) -> None:
-        self._check_data()
+    def cross_validation(self, K=10) -> None:
         metrics = self.model.crossval_hparams(
-            self.X_train, self.y_train, self.hparams, self.stage is Stage.VAL
+            self.loader.X_train,
+            self.loader.y_train,
+            self.stage is Stage.VAL,
+            K,
         )
-        self.hparams.update_cv_metrics(metrics)
+        self.cv_metrics.batch_update(metrics)
 
     def train(self) -> None:
-        self._check_data()
         metrics = self.model.fit_model(
-            self.X_train, self.y_train, self.stage is Stage.TRAIN
+            self.loader.X_train, self.loader.y_train, self.stage is Stage.TRAIN
         )
         self.training_metrics.update(metrics, len(self.X_train.index))
 
     def test(self) -> None:
         metrics = self.model.performance(
-            self.X_test, self.y_test, self.stage is Stage.TEST
+            self.loader.X_test, self.loader.y_test, self.stage is Stage.TEST
         )
         self.test_metrics.update(metrics, len(self.X_train.index))
 
     def permutation_testing(self) -> None:
-        metrics = self.model.permutation(self.X, self.y, self.stage is Stage.PERM)
+        metrics = self.model.permutation(
+            self.loader.X, self.loader.y, self.stage is Stage.PERM
+        )
         self.permutation_metrics.update(metrics)
-
-    def _check_data(self) -> None:
-        if not self.x:
-            raise AttributeError("Data has not been loaded yet.")
-
-
-def features_response(
-    data: pd.DataFrame, features: list[str], response: str
-) -> Tuple[pd.DataFrame, pd.Series]:
-    """Helper function to split data into features and responses"""
-    return data[features], data[response]
-
-
-def training_test_split(
-    X: pd.DataFrame, y: pd.Series, test_size: float, seed: int
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    """helper function for training and test splits"""
-    X_train, X_test, y_train, y_test = sk.model_selection.train_test_split(
-        X, y, test_size=test_size, random_state=seed
-    )
-    return X_train, X_test, y_train, y_test
--- tests/unit/permutation/test_runner.py
+++ tests/unit/permutation/test_runner.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import Mock
 
+import pandas as pd
+
 from permutation.runner import Runner
 from permutation.models.modelprotocol import Model
 from permutation.models.hyperparameters import Hyperparams
@@ -9,41 +11,19 @@ from permutation.loader import Loader
 
 class RunnerTests(unittest.TestCase):
     def setUp(self):
-        self.model_mock = Mock(Model)
-        self.hparam_mock = Mock(Hyperparams)
-        self.loader_mock = Mock(Loader)
+        self.model_mock = Mock(spec=Model)
+        self.loader_mock = Mock(spec=Loader)
 
     def test_stage_initialization_with_hparam(self):
         """Check to see if correct stage is set (VAL) if no hyperparameters are passed."""
+        self.model_mock.hparams = None
         testRunner = Runner(model=self.model_mock, loader=self.loader_mock)
 
         self.assertEqual(testRunner.stage.name, "TRAIN")
 
     def test_stage_initialization_without_hparam(self):
         """Check to see if correct stage is set (TRAIN) if no hyperparameters are passed."""
-        testRunner = Runner(
-            model=self.model_mock, loader=self.loader_mock, hparams=self.hparam_mock
-        )
+        self.model_mock.hparams = Mock(spec=Hyperparams)
+        testRunner = Runner(model=self.model_mock, loader=self.loader_mock)
 
         self.assertEqual(testRunner.stage.name, "VAL")
-
-    def test_loading_data_check(self):
-        """Raise AttributeError if data hasn't been loaded yet for most functions"""
-
-        def run_cv_before_loading_data():
-            testRunner = Runner(
-                model=self.model_mock, loader=self.loader_mock, hparams=self.hparam_mock
-            )
-            testRunner.cross_validation()
-
-        def run_train_before_loading_data():
-            testRunner = Runner(
-                model=self.model_mock, loader=self.loader_mock, hparams=self.hparam_mock
-            )
-            testRunner.train()
-
-        tests = [run_cv_before_loading_data, run_train_before_loading_data]
-
-        for func in tests:
-            with self.subTest(given=func):
-                self.assertRaises(AttributeError, func)
