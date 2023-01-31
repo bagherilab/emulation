72231f2acf1dd475f056bd72a8be6a28980a0831
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-01-31 13:23:01 -0800

    Refactor tests into units

--- tests/unit/permutation/experiments/test_experiment.py
+++ tests/unit/permutation/experiments/test_experiment.py
@@ -8,6 +8,8 @@ from permutation.runner import Runner
 from permutation.loader import Loader
 from permutation.experiments.experiment import StandardExperiment
 from permutation.models.hyperparameters import HParams
+from permutation.metrics import BatchMetric
+from permutation.stage import Stage
 
 @patch("permutation.loader.pd.read_csv")
 class TestStandardExperiment(unittest.TestCase):
@@ -24,13 +26,11 @@ class TestStandardExperiment(unittest.TestCase):
         self.linear_model_mock.algorithm_name = "linear_test"
         self.linear_model_mock.algorithm_abv = "MLR"
         self.linear_model_mock.hparams = Mock(spec=HParams)
-        self.linear_model_mock.performance = 1
 
-        self.rf_model_mock = Mock(spec=Model)
-        self.rf_model_mock.algorithm_name = "rf_test"
-        self.rf_model_mock.algorithm_abv = "RF"
-        self.rf_model_mock.hparams = Mock(spec=HParams)
-        self.rf_model_mock.performance = 0.5
+        self.best_linear_model_mock = Mock(spec=Model)
+        self.best_linear_model_mock.algorithm_name = "best_linear_test"
+        self.best_linear_model_mock.algorithm_abv = "MLR"
+        self.best_linear_model_mock.hparams = Mock(spec=HParams)
 
     def test_add_model(self, mock_read_csv):
         mock_read_csv.return_value = self.test_df
@@ -64,16 +64,64 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        models = [self.linear_model_mock, self.rf_model_mock]
+        models = [self.linear_model_mock, self.best_linear_model_mock]
         experiment.add_models(models)
         
         self.assertEqual(len(experiment.models), 2)
-        self.assertEqual(len(experiment.algorithms), 2)
+        self.assertEqual(len(experiment.algorithms), 1)
 
     @patch("permutation.exporter.Exporter.save_model_json")
-    def test_train_models(self, mock_save_model, mock_read_csv):
+    def test_hyperparameter_selection(self, mock_save_model, mock_read_csv):
         mock_read_csv.return_value = self.test_df
-        # mock_train.return_value = None
+
+        self.linear_model_mock_metrics = Mock(spec=BatchMetric)
+        self.linear_model_mock_metrics.stage = Stage.VAL
+        self.linear_model_mock_metrics.average = 0.5
+        self.linear_model_mock.crossval_hparams = Mock(return_value = self.linear_model_mock_metrics)
+
+        self.best_linear_model_mock_metrics = Mock(spec=BatchMetric)
+        self.best_linear_model_mock_metrics.stage = Stage.VAL
+        self.best_linear_model_mock_metrics.average = 0.9
+        self.best_linear_model_mock.crossval_hparams = Mock(return_value = self.best_linear_model_mock_metrics)
+
+        experiment = StandardExperiment(
+            self.experiment_name,
+            self.export_dir,
+            self.log_dir,
+            self.data_path,
+            self.features,
+            self.response,
+        )
+
+        experiment.add_models([self.linear_model_mock, self.best_linear_model_mock])
+        experiment.hyperparameter_selection()
+
+        expected_model = list(experiment._models.keys())[0]
+        expected_runner = str(experiment._models[expected_model][1]._UUID)
+        self.assertEqual(experiment.best_models, {expected_model: expected_runner})
+
+
+    def test_train_models(self, mock_read_csv):
+        mock_read_csv.return_value = self.test_df
+
+        experiment = StandardExperiment(
+            self.experiment_name,
+            self.export_dir,
+            self.log_dir,
+            self.data_path,
+            self.features,
+            self.response,
+        )
+
+        experiment.add_model(self.linear_model_mock)
+        experiment.train_models()
+        
+        model_key = list(experiment._models.keys())[0]
+        runner = experiment._models[model_key][0]
+
+
+    def test_test_models(self, mock_read_csv):
+        mock_read_csv.return_value = self.test_df
 
         experiment = StandardExperiment(
             self.experiment_name,
@@ -87,9 +135,10 @@ class TestStandardExperiment(unittest.TestCase):
         experiment.add_model(self.linear_model_mock)
         experiment.train_models()
         experiment.test_models()
-        experiment.hyperparameter_selection()
 
-        expected_model = list(experiment._models.keys())[0]
-        expected_runner = str(experiment._models[expected_model][0]._UUID)
-        self.assertEqual(experiment.best_models, {expected_model: expected_runner})
+        model_key = list(experiment._models.keys())[0]
+        runner = experiment._models[model_key][0]
+
+        
+    
 
