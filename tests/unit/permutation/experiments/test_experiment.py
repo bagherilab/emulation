32e4c56429fe6e9e7327ce9dcaf49366bd12150d
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-01-31 15:10:24 -0800

    More tests for training models

--- tests/unit/permutation/experiments/test_experiment.py
+++ tests/unit/permutation/experiments/test_experiment.py
@@ -22,17 +22,31 @@ class TestStandardExperiment(unittest.TestCase):
         self.features = ["x1", "x2"]
         self.response = "y"
 
-        self.linear_model_mock = Mock(spec=Model)
-        self.linear_model_mock.algorithm_name = "linear_test"
-        self.linear_model_mock.algorithm_abv = "MLR"
-        self.linear_model_mock.hparams = Mock(spec=HParams)
+        self.mock_linear_model = Mock(spec=Model)
+        self.mock_linear_model.algorithm_name = "linear_test"
+        self.mock_linear_model.algorithm_abv = "MLR"
+        self.mock_linear_model.hparams = Mock(spec=HParams)
+        self.mock_linear_model.performance = Mock(return_value=0.5)
+        self.mock_linear_model.fit_model = Mock(return_value=0.5)
 
-        self.best_linear_model_mock = Mock(spec=Model)
-        self.best_linear_model_mock.algorithm_name = "best_linear_test"
-        self.best_linear_model_mock.algorithm_abv = "MLR"
-        self.best_linear_model_mock.hparams = Mock(spec=HParams)
+        self.mock_linear_model_metrics = Mock(spec=BatchMetric)
+        self.mock_linear_model_metrics.stage = Stage.VAL
+        self.mock_linear_model_metrics.average = 0.5
+        self.mock_linear_model.crossval_hparams = Mock(return_value = self.mock_linear_model_metrics)
 
-    def test_add_model(self, mock_read_csv):
+        self.mock_best_linear_model = Mock(spec=Model)
+        self.mock_best_linear_model.algorithm_name = "best_linear_test"
+        self.mock_best_linear_model.algorithm_abv = "MLR"
+        self.mock_best_linear_model.hparams = Mock(spec=HParams)
+
+        self.mock_best_linear_model_metrics = Mock(spec=BatchMetric)
+        self.mock_best_linear_model_metrics.stage = Stage.VAL
+        self.mock_best_linear_model_metrics.average = 0.9
+        self.mock_best_linear_model.crossval_hparams = Mock(return_value = self.mock_best_linear_model_metrics)
+
+
+
+    def test_add_model_adds_model(self, mock_read_csv):
         mock_read_csv.return_value = self.test_df
         
         experiment = StandardExperiment(
@@ -44,15 +58,15 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        experiment.add_model(self.linear_model_mock)
+        experiment.add_model(self.mock_linear_model)
         
         self.assertEqual(experiment._n_models, 1)
         self.assertEqual(len(experiment.models), 1)
 
         self.assertEqual(len(experiment.algorithms), 1)
-        self.assertEqual(experiment.algorithms[0], self.linear_model_mock.algorithm_abv)
+        self.assertEqual(experiment.algorithms[0], self.mock_linear_model.algorithm_abv)
 
-    def test_add_models(self, mock_read_csv):
+    def test_add_models_adds_models(self, mock_read_csv):
         mock_read_csv.return_value = self.test_df
         
         experiment = StandardExperiment(
@@ -64,26 +78,16 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        models = [self.linear_model_mock, self.best_linear_model_mock]
+        models = [self.mock_linear_model, self.mock_best_linear_model]
         experiment.add_models(models)
         
         self.assertEqual(len(experiment.models), 2)
         self.assertEqual(len(experiment.algorithms), 1)
 
     @patch("permutation.exporter.Exporter.save_model_json")
-    def test_hyperparameter_selection(self, mock_save_model, mock_read_csv):
+    def test_hyperparameter_selection_sets_best_model(self, mock_save_model, mock_read_csv):
         mock_read_csv.return_value = self.test_df
-
-        self.linear_model_mock_metrics = Mock(spec=BatchMetric)
-        self.linear_model_mock_metrics.stage = Stage.VAL
-        self.linear_model_mock_metrics.average = 0.5
-        self.linear_model_mock.crossval_hparams = Mock(return_value = self.linear_model_mock_metrics)
-
-        self.best_linear_model_mock_metrics = Mock(spec=BatchMetric)
-        self.best_linear_model_mock_metrics.stage = Stage.VAL
-        self.best_linear_model_mock_metrics.average = 0.9
-        self.best_linear_model_mock.crossval_hparams = Mock(return_value = self.best_linear_model_mock_metrics)
-
+        
         experiment = StandardExperiment(
             self.experiment_name,
             self.export_dir,
@@ -93,15 +97,16 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        experiment.add_models([self.linear_model_mock, self.best_linear_model_mock])
+        experiment.add_models([self.mock_linear_model, self.mock_best_linear_model])
         experiment.hyperparameter_selection()
 
         expected_model = list(experiment._models.keys())[0]
         expected_runner = str(experiment._models[expected_model][1]._UUID)
         self.assertEqual(experiment.best_models, {expected_model: expected_runner})
 
-
-    def test_train_models(self, mock_read_csv):
+    @patch("permutation.exporter.Exporter.save_model_json")
+    @patch("permutation.exporter.Exporter.save_predictions")
+    def test_train_models_sets_runner_stage(self, mock_save_model, mock_save_predictions, mock_read_csv):
         mock_read_csv.return_value = self.test_df
 
         experiment = StandardExperiment(
@@ -113,14 +118,18 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        experiment.add_model(self.linear_model_mock)
+        experiment.add_model(self.mock_linear_model)
+        experiment.hyperparameter_selection()
         experiment.train_models()
         
         model_key = list(experiment._models.keys())[0]
         runner = experiment._models[model_key][0]
+        expected_stage = Stage.TEST
+        self.assertEqual(expected_stage, runner._stage)
 
-
-    def test_test_models(self, mock_read_csv):
+    @patch("permutation.exporter.Exporter.save_model_json")
+    @patch("permutation.exporter.Exporter.save_predictions")
+    def test_test_models_sets_runner_stage(self, mock_save_model, mock_save_predictions, mock_read_csv):
         mock_read_csv.return_value = self.test_df
 
         experiment = StandardExperiment(
@@ -132,13 +141,53 @@ class TestStandardExperiment(unittest.TestCase):
             self.response,
         )
 
-        experiment.add_model(self.linear_model_mock)
+        experiment.add_model(self.mock_linear_model)
+        experiment.hyperparameter_selection()
         experiment.train_models()
         experiment.test_models()
 
         model_key = list(experiment._models.keys())[0]
         runner = experiment._models[model_key][0]
+        expected_stage = Stage.PERM
+        self.assertEqual(expected_stage, runner._stage)
 
-        
     
-
+    @patch("permutation.exporter.Exporter.save_model_json")
+    @patch("permutation.exporter.Exporter.save_predictions")
+    @patch("permutation.runner.Runner.train")
+    def test_train_models_calls_runner_train(self, mock_runner_train, mock_save_prediction, mock_save_model, mock_read_csv):
+        mock_read_csv.return_value = self.test_df
+        
+        experiment = StandardExperiment(
+            self.experiment_name,
+            self.export_dir,
+            self.log_dir,
+            self.data_path,
+            self.features,
+            self.response,
+        )
+        
+        experiment.add_model(self.mock_linear_model)
+        experiment.hyperparameter_selection()
+        experiment.train_models()
+        mock_runner_train.assert_called_once()
+        
+    @patch("permutation.exporter.Exporter.save_model_json")
+    @patch("permutation.exporter.Exporter.save_predictions")
+    @patch("permutation.runner.Runner.test")
+    def test_test_models_calls_runner_test(self, mock_runner_test, mock_save_prediction, mock_save_model, mock_read_csv):
+        mock_read_csv.return_value = self.test_df
+        
+        experiment = StandardExperiment(
+            self.experiment_name,
+            self.export_dir,
+            self.log_dir,
+            self.data_path,
+            self.features,
+            self.response,
+        )
+        
+        experiment.add_model(self.mock_linear_model)
+        experiment.hyperparameter_selection()
+        experiment.test_models()
+        mock_runner_test.assert_called_once()
