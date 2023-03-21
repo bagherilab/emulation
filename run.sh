ebef330a60ce51571a09e7977ad0c1712794f885
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-03-20 17:22:21 -0700

    Updated svr with new parameter range

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 rm -rf results/ARCADE/*
-python src/config.py cs=test
-# python src/config.py cs=coupled-c
-# python src/config.py cs=coupled-chx
+python src/config.py cs=coupled-c
+python src/config.py cs=coupled-chx
 
