9537e684fdfd1f3b3db4d4d6eee054327f26e7fd
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-03-24 11:56:42 -0700

    Updated mypy

--- src/permutation/models/hyperparameters.py
+++ src/permutation/models/hyperparameters.py
@@ -6,7 +6,7 @@ from permutation.metrics import BatchMetric
 class HParams:
     """Dataclass for storing hyperparameter information and associated performance metrics."""
 
-    def __init__(self, param_dict: dict[str, Any] = None):
+    def __init__(self, param_dict: Optional[dict[str, Any]] = None):
         self.args: list[str] = []
         self.values: list[Any] = []
         self.number_of_parameters: int = 0
