af96ab66e6986566f6edf5c7beff5ac2fe806506
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-04-03 18:13:15 -0700

    New data files

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 rm -rf results/ARCADE/*
-python src/config.py cs=network-features
+python src/config.py cs=naive_0_metric_8
+python src/config.py cs=naive_0_metric_15
+
+python src/config.py cs=spatial_0_metric_8
+python src/config.py cs=spatial_0_metric_15
+
+python src/config.py cs=topo_0_metric_8
+python src/config.py cs=topo_0_metric_15
 
