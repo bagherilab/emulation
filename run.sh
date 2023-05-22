671331fc16d34fc9a28079188188048b9f6f4d94
EC2 Default User <ec2-user@ip-172-31-9-127.us-west-2.compute.internal>
2023-05-22 15:50:07 +0000

    Removed old data files

--- run.sh
+++ run.sh
@@ -2,11 +2,14 @@ rm -rf results/ARCADE/*
 # python src/config.py cs=naive_0_metric_8
 # python src/config.py cs=naive_0_metric_15
 
+python src/config.py cs=naive_8_metric_15
+python src/config.py cs=naive_15_metric_15
+
 # python src/config.py cs=topo_0_metric_8
 # python src/config.py cs=topo_0_metric_15
 
 # python src/config.py cs=spatial_0_metric_8
 # python src/config.py cs=spatial_0_metric_15
 
-python src/config.py cs/=temporal_all_metric_15_C.yaml
+# python src/config.py cs/=temporal_all_metric_15_C.yaml
 # python src/config.py cs=temporal_all_metric_15_CH.yaml
