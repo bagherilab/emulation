bdd404fec1deb315fdc890d3a963403af1a21b13
Jacob Evarts <jacob.i.evarts@gmail.com>
2023-12-13 10:46:02 -0800

    Updated config names

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 rm -rf results/ARCADE/*
-python src/config.py cs=naive_0_metric_8
-python src/config.py cs=naive_0_metric_15
+python src/config.py cs=topological_0_metric_8
+python src/config.py cs=topological_0_metric_15
 
-python src/config.py cs=topo_0_metric_8
-python src/config.py cs=topo_0_metric_15
+python src/config.py cs=hemodynamic_0_metric_8
+python src/config.py cs=hemodynamic_0_metric_15
 
 python src/config.py cs=spatial_0_metric_8
 python src/config.py cs=spatial_0_metric_15
